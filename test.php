380eab808853fe1036db5e3d19de480f91e1d8ee
yujinhai <yujinhai@yongche.com>
2015-05-21 15:35:56 +0800

    for test

--- test.php
+++ test.php
@@ -3,7 +3,6 @@
 # File Name: test.php
 # Desc: 
 # Author:root
-# mail: root@yongche.com
 # Created Time: Thu 21 May 2015 03:30:13 PM CST
 #########################################################################
 
